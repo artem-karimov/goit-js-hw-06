aaf55b0e4499daaed2fbc687e77d2cfae7fc62df
artem.karimov <artem.karimov@ecosoft.com>
2022-10-21 20:11:33 +0300

    Finish task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,34 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+'use strict';
+
+const getRandomHexColor = () => `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
+const boxes = document.getElementById('boxes');
+const inputElement = document.querySelector('input');
+const createButton = document.querySelector('[data-create');
+const destroyButton = document.querySelector('[data-destroy]');
+
+const createBoxes = amount => {
+  for (let i = 0; i < amount; i++) {
+    const divElement = document.createElement('div');
+    const divSize = `${30 + i * 10}px`;
+    divElement.style.width = divSize;
+    divElement.style.height = divSize;
+    divElement.style.backgroundColor = getRandomHexColor();
+    boxes.appendChild(divElement);
+  }
+};
+
+const destroyBoxes = () => (boxes.innerHTML = '');
+
+createButton.addEventListener('click', () => {
+  const numberOfElements = +inputElement.value;
+
+  if (numberOfElements <= 0) {
+    alert('Number must be positive!');
+    return;
+  }
+
+  createBoxes(numberOfElements);
+});
+
+destroyButton.addEventListener('click', () => destroyBoxes());
