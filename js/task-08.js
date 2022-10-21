f95bb2a994ebdb83db126c07d6d9bb895b35eead
artem.karimov <artem.karimov@ecosoft.com>
2022-10-21 19:06:29 +0300

    Finish task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+'use strict';
 
+const formElement = document.querySelector('.login-form');
+
+formElement.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const fields = {};
+
+  for (const { tagName, name, value } of formElement.elements) {
+    if (tagName === 'INPUT') {
+      if (!value) {
+        alert('All fields must be filled!');
+        return;
+      }
+      fields[name] = value;
+    }
+  }
+
+  console.log(fields);
+
+  formElement.reset();
+});
