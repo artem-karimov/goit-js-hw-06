be6dc2e6854620493e28551dfaa9089c188a0aa8
artem.karimov <artem.karimov@ecosoft.com>
2022-10-21 13:12:50 +0300

    Finish task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+'use strict';
+
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const displayIngredients = () => {
+  const items = [];
+
+  for (const ingredient of ingredients) {
+    const item = document.createElement('li');
+    item.innerHTML = ingredient;
+    item.classList.add('item');
+    items.push(item);
+  }
+
+  const ingredientsList = document.getElementById('ingredients');
+  ingredientsList.append(...items);
+};
+
+displayIngredients();
