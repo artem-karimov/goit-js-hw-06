b4a722fe6c17b035a176b288a92e53e7160e85eb
artem.karimov <artem.karimov@ecosoft.com>
2022-10-21 19:25:50 +0300

    Finish task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+'use strict';
+
+const getRandomHexColor = () => `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
+const buttonElement = document.querySelector('.change-color');
+const spanElement = document.querySelector('.color');
+
+buttonElement.addEventListener('click', () => {
+  const color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+  spanElement.innerText = color;
+});
